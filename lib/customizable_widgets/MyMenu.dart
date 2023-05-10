6a7cd880a5f38af23eb3043505f89c739752d767
Frank-128 <frankndagula12@gmail.com>
2023-05-10 10:56:39 +0300

    this is the final commit

--- lib/customizable_widgets/MyMenu.dart
+++ lib/customizable_widgets/MyMenu.dart
@@ -9,7 +9,7 @@ class MyMenu extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child:
-            Column(mainAxisAlignment: MainAxisAlignment.spaceAround, crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Column(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Center(
@@ -31,17 +31,31 @@ class MyMenu extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 MenuItem(
-                 foodMenu:const [{"food":"nyama",'price':1500},{"food":"Ndizi",'price':1000},{"food":"Chipsi",'price':2000},{"food":"Tambi",'price':3000},],
+                  foodMenu: const [
+                    {"food": "nyama", 'price': 1500},
+                    {"food": "Ndizi", 'price': 1000},
+                    {"food": "Chipsi", 'price': 2000},
+                    {"food": "Tambi", 'price': 3000},
+                  ],
                   photo: "changu-fried.jpg",
                   foodItems: "Wali",
                 ),
                 MenuItem(
-                 foodMenu:const [{"food":"Kisinia",'price':5000},{"food":"Biriani",'price':7000},],
+                  foodMenu: const [
+                    {"food": "Kisinia", 'price': 5000},
+                    {"food": "Biriani", 'price': 7000},
+                  ],
                   photo: "pilau.jpg",
                   foodItems: "Pilau",
                 ),
                 MenuItem(
-                 foodMenu:const [{"food":"Kuku",'price':8000},{"food":"Chapati",'price':500},{"food":"Vitumbua",'price':500},{"food":"Samosas",'price':1000},{"food":"Supu",'price':400},],
+                  foodMenu: const [
+                    {"food": "Kuku", 'price': 8000},
+                    {"food": "Chapati", 'price': 500},
+                    {"food": "Vitumbua", 'price': 500},
+                    {"food": "Samosas", 'price': 1000},
+                    {"food": "Supu", 'price': 400},
+                  ],
                   photo: "fries.jpeg",
                   foodItems: "Chipsi",
                 ),
@@ -53,17 +67,23 @@ class MyMenu extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 MenuItem(
-                 foodMenu:const [{"food":"nyama",'price':11},],
+                  foodMenu: const [
+                    {"food": "nyama", 'price': 11},
+                  ],
                   photo: "ndizi.JPG",
                   foodItems: "Ndizi",
                 ),
                 MenuItem(
-                 foodMenu:const [{"food":"nyama",'price':11},],
+                  foodMenu: const [
+                    {"food": "nyama", 'price': 11},
+                  ],
                   photo: "tambi_za_nyama.jpg",
                   foodItems: "Macaroni",
                 ),
                 MenuItem(
-                 foodMenu:const [{"food":"nyama",'price':11},],
+                  foodMenu: const [
+                    {"food": "nyama", 'price': 11},
+                  ],
                   photo: "nyama.jpg",
                   foodItems: "Nyama Choma",
                 ),
@@ -71,23 +91,27 @@ class MyMenu extends StatelessWidget {
             ),
           ),
           FittedBox(
-            
-
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 MenuItem(
-                 foodMenu:const [{"food":"nyama",'price':11},],
+                  foodMenu: const [
+                    {"food": "nyama", 'price': 11},
+                  ],
                   photo: "ndizi.JPG",
                   foodItems: "Ndizi",
                 ),
                 MenuItem(
-                  foodMenu:const [{"food":"nyama",'price':11},],
+                  foodMenu: const [
+                    {"food": "nyama", 'price': 11},
+                  ],
                   photo: "tambi_za_nyama.jpg",
                   foodItems: "Macaroni",
                 ),
                 MenuItem(
-                  foodMenu:const [{"food":"nyama",'price':11},],
+                  foodMenu: const [
+                    {"food": "nyama", 'price': 11},
+                  ],
                   photo: "nyama.jpg",
                   foodItems: "Nyama Choma",
                 ),
@@ -103,46 +127,84 @@ class MyMenu extends StatelessWidget {
 class MenuItem extends StatelessWidget {
   final String photo;
   final String foodItems;
-  List  <Map<String,dynamic>> foodMenu;
-  MenuItem({required this.photo, required this.foodItems,required this.foodMenu});
+  int total = 0;
+  List<Map<String, dynamic>> foodMenu;
+  MenuItem(
+      {required this.photo, required this.foodItems, required this.foodMenu});
 
   @override
   Widget build(BuildContext context) {
+    dynamic sum = addValues();
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Card(
-
-         
         child: Column(
           children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
+            Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
               Image.asset(
                 'img/' + photo,
                 width: 180,
                 height: 180,
                 fit: BoxFit.cover,
               ),
-              for(final map in foodMenu) buildFoodMenu(map),
+              for (final map in foodMenu) buildFoodMenu(map),
             ]),
-              Center(child: ElevatedButton(onPressed: (){}, child: Text('Order Now',style: TextStyle(fontSize: 18),),style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.orange[900]),))),
+            Row(
+              children: [
+                Text(
+                  'Total',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(width: 80,),
+                Text(sum.toString(), style: TextStyle(fontSize: 18, fontWeight: FontWeight.w900),)
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Order Now',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.orange[900]),
+                      ))),
+            ),
           ],
         ),
       ),
     );
   }
-  Widget buildFoodMenu(Map<String,dynamic> menu){
-    return Row(
-              
-     mainAxisAlignment: MainAxisAlignment.start,
-      children: [
 
-        Text(menu['food'],style: TextStyle(fontSize: 21),),
-        SizedBox(width: 70,),
-        Text(menu['price'].toString(),style: TextStyle(fontSize: 21),),
+  Widget buildFoodMenu(Map<String, dynamic> menu) {
+    
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Text(
+          menu['food'],
+          style: TextStyle(fontSize: 21),
+        ),
+        SizedBox(
+          width: 70,
+        ),
+        Text(
+          menu['price'].toString(),
+          style: TextStyle(fontSize: 21),
+        ),
       ],
     );
   }
-}
 
+  int addValues() {
+    dynamic total2 = 0;
+    for (var item in foodMenu) {
+      total2 += item["price"];
+    }
+
+    return total2;
+  }
+}
